84d465a31b502b67f1cbadaf38946d11eab4e041
dehre <loris4526@gmail.com>
2022-04-13 15:01:02 +0200

    task for updating BLE, same as monitor

--- main/main.c
+++ main/main.c
@@ -21,6 +21,7 @@
 #define MAX_TASK_PRIORITY (configMAX_PRIORITIES - 1U) // for reference
 #define READ_SENSOR_TASK_PRIORITY 2
 #define UPDATE_MONITOR_TASK_PRIORITY 2
+#define UPDATE_BLE_TASK_PRIORITY 2
 
 //==================================================================================================
 // ENUMS - STRUCTS - TYPEDEFS
@@ -40,12 +41,17 @@ static void read_sensor_task(void *param);
 
 static void update_monitor_task(void *param);
 
+static void update_ble_task(void *param);
+
 //==================================================================================================
 // STATIC VARIABLES
 //==================================================================================================
 
-// Pass readings from the sensor to the onboard monitor
+// Pass sensor readings from the sensor to the onboard monitor
 static QueueHandle_t mailbox_monitor = NULL;
+
+// Pass sensor readings from the sensor to the BLE peripheral
+static QueueHandle_t mailbox_ble = NULL;
 
 //==================================================================================================
 // GLOBAL FUNCTIONS
@@ -56,6 +62,7 @@ void app_main(void)
     ESP_ERROR_CHECK(ble_handler_init());
 
     mailbox_monitor = xQueueCreate(1, sizeof(sensor_reading_t));
+    mailbox_ble = xQueueCreate(1, sizeof(sensor_reading_t));
 
     TaskHandle_t periodic_task_handle = NULL;
     xTaskCreate(&read_sensor_task, "read_sensor_task", TASK_STACK_DEPTH, NULL, READ_SENSOR_TASK_PRIORITY,
@@ -66,6 +73,11 @@ void app_main(void)
     xTaskCreate(&update_monitor_task, "update_monitor_task", TASK_STACK_DEPTH, NULL, UPDATE_MONITOR_TASK_PRIORITY,
                 &update_monitor_task_handle);
     configASSERT(update_monitor_task_handle);
+
+    TaskHandle_t update_ble_task_handle = NULL;
+    xTaskCreate(&update_ble_task, "update_ble_task", TASK_STACK_DEPTH, NULL, UPDATE_BLE_TASK_PRIORITY,
+                &update_ble_task_handle);
+    configASSERT(update_ble_task_handle);
 
     vTaskDelete(NULL);
 }
@@ -89,6 +101,11 @@ static void read_sensor_task(void *param)
             ESP_LOGW(MAIN_TAG, "Last sensor reading not received from monitor, overwriting with new value");
             configASSERT(xQueueOverwrite(mailbox_monitor, (void *)&reading));
         }
+        if (xQueueSend(mailbox_ble, (void *)&reading, portMAX_DELAY) != pdPASS)
+        {
+            ESP_LOGW(MAIN_TAG, "Last sensor reading not received from BLE peripheral, overwriting with new value");
+            configASSERT(xQueueOverwrite(mailbox_ble, (void *)&reading));
+        }
     }
 }
 
@@ -101,6 +118,19 @@ static void update_monitor_task(void *param)
         if (xQueueReceive(mailbox_monitor, &reading, portMAX_DELAY))
         {
             printf("MONITOR -- temperature: %f humidity: %f\n", reading.temperature, reading.humidity);
+        }
+    }
+}
+
+static void update_ble_task(void *param)
+{
+    (void)param;
+    while (1)
+    {
+        sensor_reading_t reading;
+        if (xQueueReceive(mailbox_ble, &reading, portMAX_DELAY))
+        {
+            printf("BLE -- temperature: %f humidity: %f\n", reading.temperature, reading.humidity);
         }
     }
 }
