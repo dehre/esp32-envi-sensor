f2c88f3ff31f228f840843b60eb12d63ffb30ec2
dehre <loris4526@gmail.com>
2022-04-22 12:10:38 +0200

    workaround for ° character in ssd1306 lib

--- main/nokia_5110_lcd.c
+++ main/nokia_5110_lcd.c
@@ -12,6 +12,9 @@
 // DEFINES - MACROS
 //==================================================================================================
 
+#define MY_FONT_6x8_LEN 581 // enough memory to hold a copy of ssd1306xled_font6x8
+#define APOSTROPHE_IDX 46   // index of `'` in ssd1306xled_font6x8
+
 #define CHAR_WIDTH 6
 #define CHAR_HEIGHT 8
 #define SCREEN_WIDTH (84 / CHAR_WIDTH)
@@ -35,11 +38,21 @@
 // STATIC PROTOTYPES
 //==================================================================================================
 
+static void initialize_my_font_6x8(void);
+
 static void clear_line(uint8_t x_pos, uint8_t y_pos);
 
 //==================================================================================================
 // STATIC VARIABLES
 //==================================================================================================
+
+/*
+ * The symbol `°` is in the extended part of ASCII table and isn't included in ssd1306xled_font6x8.
+ * Without modifying the original library, we copy ssd1306xled_font6x8 into my_font_6x8 and
+ *   replace the bitmap for `'` with the bitmap for `°`.
+ * See: https://github.com/lexus2k/ssd1306/issues/139
+ */
+static uint8_t my_font_6x8[MY_FONT_6x8_LEN];
 
 //==================================================================================================
 // GLOBAL FUNCTIONS
@@ -47,7 +60,8 @@ static void clear_line(uint8_t x_pos, uint8_t y_pos);
 
 void nokia_5110_lcd_init(void)
 {
-    ssd1306_setFixedFont(ssd1306xled_font6x8);
+    initialize_my_font_6x8();
+    ssd1306_setFixedFont(my_font_6x8);
     pcd8544_84x48_spi_init(RST_PIN, CE_PIN, DC_PIN);
     ssd1306_clearScreen();
     ssd1306_printFixed(8, 8, "Envi Sensor", STYLE_ITALIC);
@@ -55,10 +69,9 @@ void nokia_5110_lcd_init(void)
 
 void nokia_5110_lcd_write_temperature(float temperature)
 {
-    // TODO LORIS: find a workaround for the `°` symbol?
     clear_line(0, 24);
     char line_buffer[SCREEN_WIDTH + 1];
-    snprintf(line_buffer, SCREEN_WIDTH + 1, "%-5s %.1f°C", "Temp:", temperature);
+    snprintf(line_buffer, SCREEN_WIDTH + 1, "%-5s %.1f'C", "Temp:", temperature);
     ssd1306_printFixed(0, 24, line_buffer, STYLE_NORMAL);
 }
 
@@ -73,6 +86,16 @@ void nokia_5110_lcd_write_humidity(float humidity)
 //==================================================================================================
 // STATIC FUNCTIONS
 //==================================================================================================
+
+static void initialize_my_font_6x8(void)
+{
+    uint8_t my_bitmap[] = {0x00, 0x00, 0x06, 0x06, 0x00, 0x00}; // bitmap for °
+    memcpy(my_font_6x8, ssd1306xled_font6x8, MY_FONT_6x8_LEN);
+    for (size_t i = APOSTROPHE_IDX, j = 0; j < sizeof(my_bitmap); i++, j++)
+    {
+        my_font_6x8[i] = my_bitmap[j];
+    }
+}
 
 static void clear_line(uint8_t x_pos, uint8_t y_pos)
 {
