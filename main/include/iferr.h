55059ff1729233d2785149f09326b55d5d3feca6
dehre <loris4526@gmail.com>
2022-04-25 15:27:08 +0200

    remove unused parentheses from iferr.h

--- main/include/iferr.h
+++ main/include/iferr.h
@@ -24,7 +24,7 @@
 #define IFERR_LOG(x, format, args...)                                                                                  \
     ({                                                                                                                 \
         esp_err_t err_rc_ = (x);                                                                                       \
-        if ((err_rc_ != ESP_OK))                                                                                       \
+        if (err_rc_ != ESP_OK)                                                                                         \
         {                                                                                                              \
             ESP_LOGE(ESP_LOG_TAG, "%s - " format ", error = %s", __func__, ##args, esp_err_to_name(err_rc_));          \
         }                                                                                                              \
@@ -36,7 +36,7 @@
 #define IFERR_RETV(x, format, args...)                                                                                 \
     ({                                                                                                                 \
         esp_err_t err_rc_ = (x);                                                                                       \
-        if ((err_rc_ != ESP_OK))                                                                                       \
+        if (err_rc_ != ESP_OK)                                                                                         \
         {                                                                                                              \
             ESP_LOGE(ESP_LOG_TAG, "%s - " format ", error = %s", __func__, ##args, esp_err_to_name(err_rc_));          \
             return;                                                                                                    \
@@ -49,7 +49,7 @@
 #define IFERR_RETE(x, format, args...)                                                                                 \
     ({                                                                                                                 \
         esp_err_t err_rc_ = (x);                                                                                       \
-        if ((err_rc_ != ESP_OK))                                                                                       \
+        if (err_rc_ != ESP_OK)                                                                                         \
         {                                                                                                              \
             ESP_LOGE(ESP_LOG_TAG, "%s - " format ", error = %s", __func__, ##args, esp_err_to_name(err_rc_));          \
             return err_rc_;                                                                                            \
